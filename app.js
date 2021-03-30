4c57b6877577ae10c12762bb51acd72889324120
HEMANT <hemant@elemensissoftech.com>
2021-03-30 16:45:28 +0530

    just messge chnage

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express =require('express');
 const app = express();
 app.get("/",function(req,res){
-    res.send('Hello HR from Docker')
+    res.send('Hello from Docker')
 })
 
 app.listen(2000,function(){
